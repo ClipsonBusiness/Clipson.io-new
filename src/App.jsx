a13e8a192878bcd991434ee81253e8ba82b9fb17
ClipsonBusiness <clipsonbusiness@gmail.com>
2025-12-19 21:02:35 +0000

    Implement routing with React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Index from './Index'
+import CampaignLaunch from './pages/CampaignLaunch'
+import SelfManagedOffer from './pages/SelfManagedOffer'
 
 function App() {
-  return <Index />
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/launch-campaign" element={<CampaignLaunch />} />
+        <Route path="/self-managed-offer" element={<SelfManagedOffer />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
-
