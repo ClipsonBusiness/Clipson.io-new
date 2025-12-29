5ac5ff4ac4614231fe9ce2ba6dc7db1f377c78b6
ClipsonBusiness <clipsonbusiness@gmail.com>
2025-12-29 22:28:21 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Index from './Index'
 import CampaignLaunch from './pages/CampaignLaunch'
 import SelfManagedOffer from './pages/SelfManagedOffer'
+import CloseSale from './pages/CloseSale'
+import Success from './pages/Success'
 
 function App() {
   return (
@@ -11,9 +13,12 @@ function App() {
         <Route path="/" element={<Index />} />
         <Route path="/launch-campaign" element={<CampaignLaunch />} />
         <Route path="/self-managed-offer" element={<SelfManagedOffer />} />
+        <Route path="/close-sale" element={<CloseSale />} />
+        <Route path="/success" element={<Success />} />
       </Routes>
     </Router>
   )
 }
 
 export default App
+
